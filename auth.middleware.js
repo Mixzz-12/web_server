4374cb282cd9f0f9b50275da4bc5c0db61b8c935
Mixzz-12 <52342@hatyaiwit.ac.th>
2025-04-17 20:15:42 +0700

    Update auth.middleware.js

--- auth.middleware.js
+++ auth.middleware.js
@@ -1,16 +1,31 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-
-
 export async function middleware(req) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-    const now = new Date().toISOString();
-    console.log(token)
-    if (!token) {
-        console.log(`[${now}] Anonymous access to ${req.nextUrl.pathname}`);
-        return NextResponse.redirect(new URL("/", req.url));
-    }
-    console.log(`[${now}] Authenticated access by ${token.name || token.email || token.sub} to ${req.nextUrl.pathname}`);
-    return NextResponse.next();
+  const isDev = process.env.NODE_ENV === "development";
+
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    cookieName: isDev
+      ? "authjs.session-token"
+      : "__Secure-authjs.session-token",
+  });
+
+  const now = new Date().toISOString();
+
+  if (!token) {
+    console.log(`[${now}] Anonymous access to ${req.nextUrl.pathname}`);
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  console.log(
+    `[${now}] Authenticated access by ${token.name || token.email || token.sub} to ${req.nextUrl.pathname}`
+  );
+  return NextResponse.next();
 }
+
+// Apply middleware only to protected routes
+export const config = {
+  matcher: ["/welcome"],
+};
